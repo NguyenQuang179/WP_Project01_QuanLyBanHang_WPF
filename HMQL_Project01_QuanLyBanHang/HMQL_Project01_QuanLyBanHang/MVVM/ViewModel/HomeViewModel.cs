17861550e6241986597b70fe0861ac90eb864186
NguyenQuang179 <79895324+NguyenQuang179@users.noreply.github.com>
2023-04-04 15:27:39 +0700

    Init Project Base Code

---
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMQL_Project01_QuanLyBanHang.Core;
+
+namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
+{
+    public class HomeViewModel
+    {
+        public HomeViewModel() {
+            
+        }
+    }
+}
---
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HMQL_Project01_QuanLyBanHang.Core;
+
+namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+        }
+    }
+}
