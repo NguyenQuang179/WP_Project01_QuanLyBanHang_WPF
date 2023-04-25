97dfad0a9055cfd5176e8bba4ee8d81cbbe17ed4
Hoang Minh <hoangminhbadao@gmail.com>
2023-04-25 23:14:59 +0700

    Create Product listing User Control And ViewModel
    undone

---
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/View/ProductListView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using BookDataBinding;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HMQL_Project01_QuanLyBanHang.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for ProductListView.xaml
+    /// </summary>
+    public partial class ProductListView : UserControl
+    {
+        private ObservableCollection<Book> _book = null;
+
+        public ProductListView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BookDAO bookDAO = new BookDAO();
+            _book = bookDAO.GetAll();
+            bookList.ItemsSource = _book;
+        }
+    }
+}
