c52baae516279df7aa408b1459153aa3e2d5386d
UnDeadCharger <rexfury121@gmail.com>
2023-04-26 11:15:08 +0700

    OrderDetailView and ViewModel Structure created

---
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/View/OrderCreateView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HMQL_Project01_QuanLyBanHang.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for OrderCreateView.xaml
+    /// </summary>
+    public partial class OrderCreateView : UserControl
+    {
+        public OrderCreateView()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/OrderDetailViewModel.cs
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/OrderDetailViewModel.cs
@@ -1,17 +1,12 @@
-﻿using HMQL_Project01_QuanLyBanHang.MVVM.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
 {
-    
-    class OrderMangementViewModel
+    class OrderDetailViewModel
     {
-        List<Order> orders = new List<Order>();
-
     }
 }
