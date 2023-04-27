b94aa50351744cd1772092c7578a04ba209c4077
Hoang Minh <hoangminhbadao@gmail.com>
2023-04-27 10:36:34 +0700

    Create Product View and Product Add View (done)

--- HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/View/ProductListView.xaml.cs
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/View/ProductListView.xaml.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -34,6 +35,63 @@ namespace HMQL_Project01_QuanLyBanHang.MVVM.View
             BookDAO bookDAO = new BookDAO();
             _book = bookDAO.GetAll();
             bookList.ItemsSource = _book;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            string searchTerm = searchTextBox.Text;
+            // Perform search logic here
+        }
+
+        private void searchTextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            searchTextBox.Text = "";
+        }
+
+        private void searchTB_MouseEnter(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void searchTB_MouseLeave(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void eraseButton_Click(object sender, RoutedEventArgs e)
+        {
+            searchTextBox.Text = "";
+            Keyboard.ClearFocus();
+        }
+
+        private void sortButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private static readonly Regex _regex = new Regex("[^0-9.-]+"); //regex that matches disallowed text
+
+        private static bool IsTextAllowed(string text)
+        {
+            return !_regex.IsMatch(text);
+        }
+
+        private void PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !IsTextAllowed(e.Text);
+        }
+
+        private void TextBoxPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            if (e.DataObject.GetDataPresent(typeof(String)))
+            {
+                String text = (String)e.DataObject.GetData(typeof(String));
+                if (!IsTextAllowed(text))
+                {
+                    e.CancelCommand();
+                }
+            }
+            else
+            {
+                e.CancelCommand();
+            }
         }
     }
 }
---
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/View/ProductView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HMQL_Project01_QuanLyBanHang.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for ProductView.xaml
+    /// </summary>
+    public partial class ProductView : UserControl
+    {
+        public ProductView()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
