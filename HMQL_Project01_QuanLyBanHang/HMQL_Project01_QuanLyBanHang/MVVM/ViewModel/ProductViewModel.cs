927f3edbd5a4a62f2dac07abbe99feddcd14eb7a
Hoang Minh <hoangminhbadao@gmail.com>
2023-04-28 20:04:56 +0700

    Revert "Revert "Update Product List/Add/View""
    This reverts commit fec35e9cd79b99acdcec39550311f920745d2c6f.

--- HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/ProductViewModel.cs
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/ProductViewModel.cs
@@ -6,18 +6,106 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Runtime.Serialization;
 using System.Collections.ObjectModel;
+using Microsoft.Win32;
+using System.Windows.Media.Imaging;
 
 namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
 {
     internal class ProductViewModel : ObservableObject
     {
-        public ProductViewModel()
+        private string bookName;
+
+        public string BookName
         {
-            //ObservableCollection<Book> _book = null;
-            //// this.DataSource = sv;
-            //BookDAO bookDAO = new BookDAO();
-            //_book = bookDAO.GetAll();
-            //bookList.ItemsSource = _book;
+            get => bookName;
+            set
+            {
+                bookName = value;
+                OnPropertyChanged(nameof(BookName));
+            }
+        }
+
+        private string author;
+
+        public string Author
+        {
+            get => author;
+            set
+            {
+                author = value;
+                OnPropertyChanged(nameof(Author));
+            }
+        }
+
+        private string category;
+
+        public string Category
+        {
+            get => category;
+            set
+            {
+                category = value;
+                OnPropertyChanged(nameof(Category));
+            }
+        }
+
+        private string publishedYear;
+
+        public string PublishedYear
+        {
+            get => publishedYear;
+            set
+            {
+                publishedYear = value;
+                OnPropertyChanged(nameof(PublishedYear));
+            }
+        }
+
+        private int price;
+
+        public int Price
+        {
+            get => price;
+            set
+            {
+                price = value;
+                OnPropertyChanged(nameof(Price));
+            }
+        }
+
+        private int stock;
+
+        public int Stock
+        {
+            get => stock;
+            set
+            {
+                stock = value;
+                OnPropertyChanged(nameof(Stock));
+            }
+        }
+
+        public RelayCommand BackCommand { get; set; }
+
+        public RelayCommand SaveBookToAPICommand { get; set; }
+
+        public RelayCommand BrowseImage { get; set; }
+
+        public ProductViewModel(MainViewModel mainVM)
+        {
+            BackCommand = new RelayCommand(o =>
+            {
+                mainVM.ProductListViewCommand.Execute(null);
+            });
+
+            BrowseImage = new RelayCommand(o =>
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Filter = "Image files (*.png;*.jpeg;*.jpg)|*.png;*.jpeg;*.jpg|All files (*.*)|*.*";
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    BookImage.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                }
+            });
         }
     }
-}
