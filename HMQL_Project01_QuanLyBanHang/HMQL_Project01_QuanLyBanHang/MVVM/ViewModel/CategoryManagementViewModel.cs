d9be127731763d2f3d39378c4618efb0885757ff
UnDeadCharger <rexfury121@gmail.com>
2023-04-29 12:10:26 +0700

    Added Command For Group Category View

--- HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/CategoryManagementViewModel.cs
+++ HMQL_Project01_QuanLyBanHang/HMQL_Project01_QuanLyBanHang/MVVM/ViewModel/CategoryManagementViewModel.cs
@@ -11,6 +11,7 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
 {
@@ -29,6 +30,7 @@ namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
         public Category SelectedCategory { get; set; }
         public RelayCommand CategoryDetailViewCommand { get; set; }
         public RelayCommand CategoryCreateViewCommand { get; set; }
+        public RelayCommand DeleteDataCommand { get; set; }
 
         private ListCategory data;
         public ListCategory Data
@@ -117,7 +119,7 @@ namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
         public RelayCommand NextPageCommand { get; set; }
         public RelayCommand PrevPageCommand { get; set; }
         public CategoryDetailViewModel CategoryDetailVM { get; set; }
-        public CategoryCreateViewModel CategoryCreateVM { get; private set; }
+        public CategoryCreateViewModel CategoryCreateVM { get; set; }
 
         public CategoryManagementViewModel(MainViewModel MainVM) {
             CurPage = 1;
@@ -145,6 +147,61 @@ namespace HMQL_Project01_QuanLyBanHang.MVVM.ViewModel
                 CategoryCreateVM = new CategoryCreateViewModel(MainVM);
                 MainVM.CurrentView = CategoryCreateVM;
             });
+
+            DeleteDataCommand = new RelayCommand(async (param) =>
+            {
+                string categoryID = param as string;
+                MessageBox.Show(categoryID);
+                //Delete Chosen Category, nhớ check null cho categoryID
+
+                //Bỏ cái này vô sau khi chạy API thành công
+                CallData.Execute(null);
+              
+            });
+
+            //DeleteDataCommand = new RelayCommand(async (param) =>
+            //{
+            //    string categoryID = param as string;
+            //    MessageBox.Show(categoryID);
+            //    //List<Order> curList = Orders.listOfOrder;
+            //    //// Find the index of the book to remove
+
+            //    //string orderId = param as string;
+            //    //if (orderId == null)
+            //    //{
+            //    //    //show error
+            //    //    MessageBox.Show("Invalid Order ID");
+            //    //    return;
+            //    //}
+            //    //// Find the index of the book to remove
+            //    //int indexToRemove = curList.FindIndex(b => b._id == orderId);
+
+
+            //    //if (indexToRemove != -1)
+            //    //{
+            //    //    // Remove the book from curList
+            //    //    using var client = new HttpClient();
+            //    //    var uri = new Uri($"{ConnectionString.connectionString}/order/delete/{orderId}");
+            //    //    var response = await client.DeleteAsync(uri);
+
+            //    //    // Check if the upload was successful
+            //    //    if (response.IsSuccessStatusCode)
+            //    //    {
+            //    //        var json = await response.Content.ReadAsStringAsync();
+            //    //        MessageBox.Show(json);
+            //    //        //MessageBox.Show($"{Orders.listOfOrder.Count} {newdate}");
+            //    //    }
+            //    //    else { MessageBox.Show($"Fail To Delete Order"); }
+            //    //    TotalOrder--;
+            //    //    UpdatePageDataCommand.Execute(null);
+
+            //    //}
+            //    //else
+            //    //{
+            //    //    //show error
+            //    //    MessageBox.Show("No Order Selected");
+            //    //}
+            //});
 
             CallData = new RelayCommand(async o =>
             {
